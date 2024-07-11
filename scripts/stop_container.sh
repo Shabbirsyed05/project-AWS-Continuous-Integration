0720093c610793c629b937bab25f09a128077cea
Shabbirsyed05 <119849465+Shabbirsyed05@users.noreply.github.com>
2024-07-11 16:05:39 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,12 @@
 set -e
 
 # Stop the running container (if any)
-containerid=`docker ps | awk -F " " '{print $1}' `
-docker rm -f $containerid
+
+#containerid=`docker ps | awk -F " " '{print $1}' `
+#docker rm -f $containerid
+
+# Stop all running Docker containers
+docker stop $(docker ps -aq)
+
+# Remove all stopped Docker containers
+docker rm $(docker ps -aq)
